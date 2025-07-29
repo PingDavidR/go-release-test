9c9241811c5ccb81d6aff29d8ceb506caf84b116
PingDavidR <davidross@pingidentity.com>
2025-07-29 08:30:36 -0500

    feat: add CLI support for power, sqrt, sin, and cos operations

--- cmd/mathreleaser/main.go
+++ cmd/mathreleaser/main.go
@@ -15,7 +15,7 @@ import (
 func main() {
 	// Define command-line flags
 	versionFlag := flag.Bool("version", false, "Print version information")
-	operation := flag.String("op", "add", "Operation to perform: add, subtract, multiply, divide")
+	operation := flag.String("op", "add", "Operation to perform: add, subtract, multiply, divide, power, sqrt, sin, cos")
 
 	// Parse command-line flags
 	flag.Parse()
@@ -26,23 +26,43 @@ func main() {
 		return
 	}
 
-	// Check if we have the required number of arguments
+	// Check arguments based on operation
 	args := flag.Args()
-	if len(args) != 2 {
-		fmt.Println("Usage: mathreleaser -op=[add|subtract|multiply|divide] <number1> <number2>")
-		fmt.Println("       mathreleaser -version")
-		os.Exit(1)
-	}
+	var a, b float64
+	var err error
 
-	// Parse the input numbers
-	a, err := strconv.ParseFloat(args[0], 64)
-	if err != nil {
-		helpers.PrintError("Error parsing first number: %v", err)
-	}
-
-	b, err := strconv.ParseFloat(args[1], 64)
-	if err != nil {
-		helpers.PrintError("Error parsing second number: %v", err)
+	switch *operation {
+	case "add", "subtract", "multiply", "divide", "power":
+		if len(args) != 2 {
+			fmt.Println("Usage: mathreleaser -op=[add|subtract|multiply|divide|power] <number1> <number2>")
+			fmt.Println("       mathreleaser -version")
+			os.Exit(1)
+		}
+		a, err = strconv.ParseFloat(args[0], 64)
+		if err != nil {
+			helpers.PrintError("Error parsing first number: %v", err)
+		}
+		b, err = strconv.ParseFloat(args[1], 64)
+		if err != nil {
+			helpers.PrintError("Error parsing second number: %v", err)
+		}
+	case "sqrt", "sin", "cos":
+		if len(args) != 1 {
+			fmt.Println("Usage: mathreleaser -op=[sqrt|sin|cos] <number>")
+			fmt.Println("       mathreleaser -version")
+			os.Exit(1)
+		}
+		a, err = strconv.ParseFloat(args[0], 64)
+		if err != nil {
+			helpers.PrintError("Error parsing number: %v", err)
+		}
+	default:
+		if len(args) == 0 {
+			fmt.Println("Usage: mathreleaser -op=[add|subtract|multiply|divide|power] <number1> <number2>")
+			fmt.Println("       mathreleaser -op=[sqrt|sin|cos] <number>")
+			fmt.Println("       mathreleaser -version")
+			os.Exit(1)
+		}
 	}
 
 	// Perform the requested operation
@@ -65,6 +85,21 @@ func main() {
 			helpers.PrintError("Error performing division: %v", opErr)
 		}
 		fmt.Printf("%s / %s = %s\n", args[0], args[1], helpers.FormatNumber(result))
+	case "power":
+		result = calculator.Power(a, b)
+		fmt.Printf("%s ^ %s = %s\n", args[0], args[1], helpers.FormatNumber(result))
+	case "sqrt":
+		result, opErr = calculator.SquareRoot(a)
+		if opErr != nil {
+			helpers.PrintError("Error performing square root: %v", opErr)
+		}
+		fmt.Printf("sqrt(%s) = %s\n", args[0], helpers.FormatNumber(result))
+	case "sin":
+		result = calculator.Sin(a)
+		fmt.Printf("sin(%s) = %s\n", args[0], helpers.FormatNumber(result))
+	case "cos":
+		result = calculator.Cos(a)
+		fmt.Printf("cos(%s) = %s\n", args[0], helpers.FormatNumber(result))
 	default:
 		helpers.PrintError("Unknown operation: %s", *operation)
 	}
